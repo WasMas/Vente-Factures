2a7fd28d11c9ffbb65903cc7af265a8f9d57c30c
Wassim Masmoudi <106668591+WasMas@users.noreply.github.com>
2023-12-02 16:32:56 +0100

    Fixed the DataBase

--- src/Entr.2.c
+++ src/Entr.2.c
@@ -6,32 +6,41 @@ typedef struct
 {
   int idFacture;
   int codeClient;
-  char *montant;
+  char montant[100];
 } VentesVoitures;
 
 int main(int argc, char *argv[])
+
+
 {
-  sqlite3_stmt *statement = NULL;
-  VentesVoitures ventes[255];
+  //Opening DataBase
   sqlite3 *db;
-  int rc;
-  int i = 0;
-  char *sql = "SELECT * FROM ventesVoitures";
-
   sqlite3_open("../sockets.db", &db);
-  rc = sqlite3_prepare_v2(db, sql, -1, &statement, NULL);
 
-  do
-  {
+
+  sqlite3_stmt *statement, *statementRow = NULL;
+  VentesVoitures ventes[255];
+
+  int rc,rc1,rowCount;
+  char* sql = "SELECT * FROM ventesVoitures";
+  char* sqlRowCount="SELECT count(*) from ventesVoitures";
+  
+  rc = sqlite3_prepare_v2(db, sql, -1, &statement, NULL);
+  rc1 = sqlite3_prepare_v2(db, sqlRowCount, -1, &statementRow, NULL);
+  
+  rc1 = sqlite3_step(statementRow);
+  rowCount = sqlite3_column_int(statementRow, 0);
+
+
+  for (int i=0; i<rowCount;i++){
     rc = sqlite3_step(statement);
     ventes[i].codeClient = sqlite3_column_int(statement, 0);
     ventes[i].idFacture = sqlite3_column_int(statement, 1);
-    ventes[i].montant = sqlite3_column_text(statement, 2);
-    i++;
-  } while (rc != SQLITE_DONE);
+    strcpy(ventes[i].montant,sqlite3_column_text(statement, 2));
+  }
+  
+  printf("%i %i %s\n", ventes[2].codeClient, ventes[2].idFacture, ventes[0].montant);
 
-  printf("%i %i %s\n", ventes[1].codeClient, ventes[1].idFacture, ventes[1].montant);
-  printf("%i %i %s\n", ventes[0].codeClient, ventes[0].idFacture, ventes[0].montant);
   sqlite3_finalize(statement);
   sqlite3_close(db);
 
