90b800618bc8f7f84cdbd058d6bddba5a5af8533
Wassim Masmoudi <106668591+WasMas@users.noreply.github.com>
2023-12-02 20:08:05 +0100

    Communication between Entr.2 and proxy

--- src/Entr.2.c
+++ src/Entr.2.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <sqlite3.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#include <netinet/in.h>
 
 typedef struct
 {
@@ -11,6 +16,7 @@ typedef struct
 
 int main()
 {
+  // ? Gettin Data From DataBase
   // Opening DataBase
   sqlite3 *db;
   sqlite3_open("../sockets.db", &db);
@@ -36,10 +42,34 @@ int main()
     strcpy(ventes[i].montant, sqlite3_column_text(statement, 2));
   }
 
-  printf("%i %i %s\n", ventes[2].codeClient, ventes[2].idFacture, ventes[0].montant);
-
   sqlite3_finalize(statement);
   sqlite3_close(db);
 
+// ? Socket Stuff
+  // Seding Struct To Proxy server using TCP
+  int Entr2_socket, new_socket;
+  struct sockaddr_in address;
+  int opt = 1;
+  int addrlen = sizeof(address);
+
+  // Creating socket
+  Entr2_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+  // Configuring socket
+  address.sin_family = AF_INET;
+  address.sin_addr.s_addr = INADDR_ANY;
+  address.sin_port = htons(9002);
+
+  // Attaching socket to port 
+  bind(Entr2_socket, (struct sockaddr *)&address, sizeof(address));
+  
+  // Listening To connection
+  listen(Entr2_socket, 5);
+
+  new_socket = accept(Entr2_socket, (struct sockaddr *)&address, (socklen_t *)&addrlen);
+
+  // Sending the struct
+  send(new_socket, ventes, sizeof(ventes), 0);
+  printf("Entr.2: Array of structs sent\n");
   return 0;
 }
---
+++ src/proxyServer.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+typedef struct
+{
+    int idFacture;
+    int codeClient;
+    char montant[255];
+} VentesVoitures;
+
+int main()
+{
+    struct sockaddr_in proxy_address;
+    int proxy_socket = 0;
+    VentesVoitures ventes[255];
+
+proxy_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+proxy_address.sin_family = AF_INET;
+proxy_address.sin_port = htons(9002);
+
+connect(proxy_socket, (struct sockaddr *)&proxy_address, sizeof(proxy_address));
+
+// Receiving the array of structs
+recv(proxy_socket, ventes, sizeof(ventes), 0);
+
+printf("Received array of structs From Entr.2:\n");
+for (int i = 0; i < 3; ++i)
+{
+    printf("id %i facture: %i, montant: %s,", ventes[i].idFacture, ventes[i].idFacture, ventes[i].montant);
+}
+return 0;
+}
