aecfde2e78f77d72b722e033d949fea2cccea813
Wassim Masmoudi <106668591+WasMas@users.noreply.github.com>
2023-12-02 13:06:44 +0100

    databse linking works
    only retrieving values from the string column needs fix

--- src/Entr.2.c
+++ src/Entr.2.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-#include <sqlite3.h> 
+#include <sqlite3.h>
+#include <string.h>
 
 typedef struct
 {
@@ -8,13 +9,31 @@ typedef struct
   char *montant;
 } VentesVoitures;
 
-int main(int argc, char* argv[]) {
-   sqlite3 *db;
-   int rc;
-   char *sql;
-   char *ErrMsg = 0;
-   rc = sqlite3_open("../sockets.db", &db);
-   sql="SELECT * FROM ventesVoitures";
-   rc = sqlite3_exec(db, sql, qsd, 0, &ErrMsg);
-sqlite3_close(db);
+int main(int argc, char *argv[])
+{
+  sqlite3_stmt *statement = NULL;
+  VentesVoitures ventes[255];
+  sqlite3 *db;
+  int rc;
+  int i = 0;
+  char *sql = "SELECT * FROM ventesVoitures";
+
+  sqlite3_open("../sockets.db", &db);
+  rc = sqlite3_prepare_v2(db, sql, -1, &statement, NULL);
+
+  do
+  {
+    rc = sqlite3_step(statement);
+    ventes[i].codeClient = sqlite3_column_int(statement, 0);
+    ventes[i].idFacture = sqlite3_column_int(statement, 1);
+    ventes[i].montant = sqlite3_column_text(statement, 2);
+    i++;
+  } while (rc != SQLITE_DONE);
+
+  printf("%i %i %s\n", ventes[1].codeClient, ventes[1].idFacture, ventes[1].montant);
+  printf("%i %i %s\n", ventes[0].codeClient, ventes[0].idFacture, ventes[0].montant);
+  sqlite3_finalize(statement);
+  sqlite3_close(db);
+
+  return 0;
 }
