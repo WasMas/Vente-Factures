0b38ba2aff48045cee5370be653292b1104e00d7
Wassim Masmoudi <106668591+WasMas@users.noreply.github.com>
2023-12-03 16:17:59 +0100

    Proxy Server Listens to both TCP and UDP requests

--- src/proxyServer.c
+++ src/proxyServer.c
@@ -4,6 +4,10 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <stdbool.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <pthread.h>
 typedef struct
 {
     int idFacture;
@@ -11,7 +15,47 @@ typedef struct
     char montant[255];
 } VentesVoitures;
 
-int main()
+typedef struct
+{
+    int idFacture;
+    int codeClient;
+    char montant[256];
+} VentesPara;
+
+void *Entr1Config(void *arg)
+{
+    int proxy_socket_UDP, len;
+    struct sockaddr_in serverAddr_UDP, clientAddr;
+    VentesPara ventes[256];
+
+    // Creating socket
+    proxy_socket_UDP = socket(AF_INET, SOCK_DGRAM, 0);
+    serverAddr_UDP.sin_family = AF_INET;
+    serverAddr_UDP.sin_addr.s_addr = INADDR_ANY;
+    serverAddr_UDP.sin_port = htons(9001);
+
+    // Binding the socket
+    bind(proxy_socket_UDP, (const struct sockaddr *)&serverAddr_UDP, sizeof(serverAddr_UDP));
+
+    len = sizeof(clientAddr);
+    while (1)
+    {
+        recvfrom(proxy_socket_UDP, ventes, sizeof(ventes),
+                 MSG_WAITALL, (struct sockaddr *)&clientAddr, &len);
+
+        printf("Entr1 Sent Stuff:\n");
+        // Processing received data (Here you can perform any operations with the received structures)
+        for (int i = 0; i < 3; i++)
+        {
+            printf("ID: %i, Value: %i, Message: %s\n",
+                   ventes[i].codeClient, ventes[i].idFacture, ventes[i].montant);
+        }
+    }
+    close(proxy_socket_UDP);
+    return NULL;
+}
+
+void *Entr2Config(void *arg)
 {
     struct sockaddr_in proxy_address_tcp;
     int proxy_socket, new_socket;
@@ -34,6 +78,7 @@ int main()
         new_socket = accept(proxy_socket, (struct sockaddr *)&proxy_address_tcp, (socklen_t *)&addrlen);
         // Receiving the array of structs
         recv(new_socket, ventes, sizeof(ventes), 0);
+        printf("Entr2 Sent Stuff:\n");
         for (int i = 0; i < 3; ++i)
         {
             printf("id %i facture: %i, montant: %s,", ventes[i].idFacture, ventes[i].idFacture, ventes[i].montant);
@@ -42,5 +87,20 @@ int main()
     }
     close(proxy_socket);
     close(new_socket);
+    return NULL;
+}
+
+int main()
+{
+    pthread_t entr1Thread, entr2Thread;
+
+    // Creating threads for handling ENTR1 and ENTR2 connections
+    pthread_create(&entr1Thread, NULL, Entr1Config, NULL);
+    pthread_create(&entr2Thread, NULL, Entr2Config, NULL);
+
+    // Waiting for threads to finish
+    pthread_join(entr1Thread, NULL);
+    pthread_join(entr2Thread, NULL);
+
     return 0;
 }
